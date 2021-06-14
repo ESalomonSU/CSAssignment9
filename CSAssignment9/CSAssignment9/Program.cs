611729445af9c03d09a10d0a9c81d4c837ff9d9c
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-06-14 01:30:01 -0600

    CS Assignment 9

---
+++ CSAssignment9/CSAssignment9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSAssignment9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NormalClass NewClass = new NormalClass();
+
+            NewClass.GenericMethod<string>(11, "Eleven");
+        }
+    }
+}
+
+public class NormalClass
+{
+    public void GenericMethod<T>(int parameter1, T parameter2)
+    {
+        Console.WriteLine("The parameters:");
+        Console.WriteLine(parameter1);
+        Console.WriteLine(parameter2);
+    }
+}
